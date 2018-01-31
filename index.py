ce80770ce4de6f8152a7249b22470f3b8aef8871
Vinicius-Trindade <36005122+Vinicius-Trindade@users.noreply.github.com>
2018-01-31 15:10:30 -0200

    Add files via upload

---
+++ index.py
@@ -0,0 +1,20 @@
+def func1(x, y, z):
+    print("example")
+    return x * y * z
+print(func1(3, 4, 5))
+print("limit")
+
+b = [1, 2, 3]
+total = 0
+for a in b:
+    total = total + a
+print(total)
+
+a = list(range(1, 100))
+for  ß in a:
+    if ß % 3 == 0:
+        print("Multiplo de 3")
+        print(ß)
+    elif ß % 5 == 0:
+        print("Multiplo de 5")
+        print(ß)
