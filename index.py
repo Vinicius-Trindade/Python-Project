ffcac863eb49fdb7f53530baf891124b926c0522
Vinicius-Trindade <36005122+Vinicius-Trindade@users.noreply.github.com>
2018-03-01 21:52:10 -0300

    Update index.py

--- index.py
+++ index.py
@@ -3,6 +3,11 @@ def func1(x, y, z):
     return x * y * z
 print(func1(3, 4, 5))
 
+def convert(miles):
+    kilometers = miles * 1.60934
+    return "miles: " + str(miles) + " = " "kilometers: " + str(kilometers)
+print(convert(1))
+
 b = [1, 2, 3]
 total = 0
 for a in b:
